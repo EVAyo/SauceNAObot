5b2e44089c71e4d8f4893e3f73a104b0aa5b6248
Quetzal Rivera <QuetzalDeveloper@outlook.com>
2022-01-14 18:26:19 -0600

    App architecture was improved

---
+++ src/SauceNao.Core/ISauce.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SauceNAO.Core
+{
+    public interface ISauce
+    {
+        string? Title { get; }
+        string? Characters { get; }
+        string? Material { get; }
+        string? Part { get; }
+        string? Year { get; }
+        string? EstTime { get; }
+        string? By { get; }
+    }
+}
